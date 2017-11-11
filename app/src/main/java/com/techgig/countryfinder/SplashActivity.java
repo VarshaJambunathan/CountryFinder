0c805e872d8735ab4b4158aed4669a45dd34a496
VarshaJambunathan <jvarsha621@gmail.com>
2017-11-11 14:42:48 +0530

    SplashActivity: Added Timeout function

--- app/src/main/java/com/techgig/countryfinder/SplashActivity.java
+++ app/src/main/java/com/techgig/countryfinder/SplashActivity.java
@@ -1,13 +1,39 @@
 package com.techgig.countryfinder;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private TextView mSearchCountry;
+    private static int SPLASH_TIME_OUT = 750;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mSearchCountry = (TextView)findViewById(R.id.search_country);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app register activity for a new user
+                //Intent login = new Intent(SplashActivity.this, LoginActivity.class);
+                //startActivity(login);
+
+                //Else handle the cookies/preferences to navigate to app main activity
+                //Code here!
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
