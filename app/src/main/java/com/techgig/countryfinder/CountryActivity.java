6b2ff5d2f234c804759f0facea617ea698faec95
VarshaJambunathan <jvarsha621@gmail.com>
2017-11-12 15:06:48 +0530

    Country RecyclerView Layouts ready.

--- app/src/main/java/com/techgig/countryfinder/CountryActivity.java
+++ app/src/main/java/com/techgig/countryfinder/CountryActivity.java
@@ -2,15 +2,22 @@ package com.techgig.countryfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.techgig.countryfinder.Adapters.CountryAdapter;
 import com.techgig.countryfinder.Beans.Country;
 
 import java.util.ArrayList;
 
 public class CountryActivity extends AppCompatActivity {
 
-    ArrayList<Country> mCountryNames;
+    private ArrayList<Country> mCountryNames;
+    private RecyclerView mCountryView;
+    private RecyclerView.LayoutManager mCountryLayoutManager;
+    private RecyclerView.Adapter mCountryAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +25,16 @@ public class CountryActivity extends AppCompatActivity {
 
         mCountryNames = new ArrayList<>();
         mCountryNames.add(new Country("India", "IN", "New Delhi", "Hindi"));
-        mCountryNames.add(new Country("India", "IN", "New Delhi", "Hindi"));
-        mCountryNames.add(new Country("India", "IN", "New Delhi", "Hindi"));
+        mCountryNames.add(new Country("Israel", "IN", "New Delhi", "Hindi"));
+        mCountryNames.add(new Country("United States", "IN", "New Delhi", "Hindi"));
+
+        //Country RecyclerView
+        mCountryView = (RecyclerView) findViewById(R.id.country_view);
+        mCountryView.setHasFixedSize(true);
+        mCountryLayoutManager = new LinearLayoutManager(this);
+        mCountryView.setLayoutManager(mCountryLayoutManager);
+        mCountryAdapter =  new CountryAdapter(mCountryNames);
+        mCountryView.setAdapter(mCountryAdapter);
 
         Log.e("SplashActivity", mCountryNames.get(1).getCapital());
 
